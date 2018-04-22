59de3df292285ebd943c1d104c63f458fd25511f
rshynybek <38507353+rshynybek@users.noreply.github.com>
2018-04-22 14:16:33 -0500

    Add files via upload

--- Final Project/assets/js/animation.js
+++ Final Project/assets/js/animation.js
@@ -1,18 +1,68 @@
-var canvas = document.getElementById('drawing');
-var context = canvas.getContext('2d');
+var sun = new Image();
+var mercury = new Image();
+var venus = new Image();
+var earth = new Image();
+function init() {
+  sun.src = 'https://mdn.mozillademos.org/files/1456/Canvas_sun.png';
+  mercury.src = './assets/images/mercury.jpg';
+  venus.src = 'https://mdn.mozillademos.org/files/1443/Canvas_moon.png';
+  earth.src ='./assets/images/Canvas_earth.png';
+  window.requestAnimationFrame(draw);
+}
 
-context.fillStyle="white";
-context.fillRect(0,0,150,75);
-context.moveTo(150,50);
-context.stroke();
+function draw() {
+  var canvas = document.getElementById('drawing');
+  var context = canvas.getContext('2d');
 
-var pos = 0;
-context.fillRect(pos,0,40,40);
-setInterval(function() {
-  context.clearRect(0,0,400,400);
-  context.fillRect(pos,0,40,40);
-  pos++;
-  if (pos>400) {
-    pos = 0;
-  }
-},15);
+  context.globalCompositeOperation = 'destination-over';
+  context.clearRect(0,0,500,500);
+
+  context.fillStyle = 'rgba(0, 0, 0, 0.4)';
+  context.strokeStyle = 'rgba(0, 150, 250, 5)';
+  context.save();
+  context.translate(300,300);
+
+//mercury
+  var time = new Date();
+  context.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  context.translate(90,0);
+  context.fillRect(0,0,0,0);
+  context.scale(0.05,0.05);
+  context.drawImage(mercury,-12,-12);
+
+  //venus
+  var time = new Date();
+  context.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  context.translate(150,0);
+  context.fillRect(0,0,0,0);
+  context.scale(100,100);
+  context.drawImage(venus,-12,-12);
+
+  //earth
+  var time = new Date();
+  context.rotate(((2 * Math.PI) / 60) * time.getSeconds() + ((2 * Math.PI) / 60000) * time.getMilliseconds());
+  context.translate(200,0);
+  context.fillRect(0,0,0,0);
+  context.drawImage(earth,-12,-12);
+
+  context.restore();
+
+  context.beginPath();
+  context.arc(300,300,105,0,Math.PI*2,false);
+  context.stroke();
+
+  context.beginPath();
+  context.arc(300,300,150,0,Math.PI*2,false);
+  context.stroke();
+
+  context.beginPath();
+  context.arc(300,300,200,0,Math.PI*2,false);
+  context.stroke();
+
+  context.drawImage(sun,150,150,300,300);
+
+  window.requestAnimationFrame(draw);
+
+}
+
+init();
